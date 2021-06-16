a1c3cc2e2fd2cc32bf5759de299a5f893a17b147
Oleksandr Kryvonos <geraclion@gmail.com>
2021-06-17 01:15:32 +0300

    first commit on a long way

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ast_lispy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
