328530c22d1575d6342e2e08dcaee0c7fd75eb00
Oleksandr Kryvonos <geraclion@gmail.com>
2021-06-17 20:00:32 +0300

    more cleanup + knockoutjs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var lookup = {};
+lookup.createFunction = function()
+{
+    var guid = lookup.uuidv4();
+    lookup[guid] = 
+    {
+        operation: "define-function"
+    };
+    console.log("createFunction");
+};
+
+lookup.uuidv4 = function() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  
