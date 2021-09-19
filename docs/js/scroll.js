edb859a89c3501223edd7e0b23fc074905f3a40e
Oleksandr Kryvonos <geraclion@gmail.com>
2021-09-19 11:24:43 +0300

    added boundaries for scroll

--- docs/js/scroll.js
+++ docs/js/scroll.js
@@ -5,11 +5,22 @@ if(typeof(lookup) === 'undefined')
 lookup.globalOffsetX = ko.observable(0.0);
 lookup.globalOffsetY = ko.observable(0.0);
 lookup.globalOffsetZ = ko.observable(1.0);
+lookup.globalMaxX = ko.observable(2048);
+lookup.globalMaxY = ko.observable(2048);
+lookup.globalMinX = ko.observable(-2048);
+lookup.globalMinY = ko.observable(-2048);
+
 lookup.bodyOnWheel = function() {
     event.preventDefault();
     console.log(event);
-    lookup.globalOffsetY( lookup.globalOffsetY() +  event.deltaY * lookup.globalOffsetZ() );
-    lookup.globalOffsetX( lookup.globalOffsetX() +  event.deltaX * lookup.globalOffsetZ() );
+    var newOffsetY = lookup.globalOffsetY() - event.deltaY * lookup.globalOffsetZ();
+    newOffsetY = Math.min(newOffsetY, lookup.globalMaxY());
+    newOffsetY = Math.max(newOffsetY, lookup.globalMinY());
+    lookup.globalOffsetY( newOffsetY );
+    var newOffsetX = lookup.globalOffsetX() - event.deltaX * lookup.globalOffsetZ();
+    newOffsetX = Math.min(newOffsetX, lookup.globalMaxX());
+    newOffsetX = Math.max(newOffsetX, lookup.globalMinX());
+    lookup.globalOffsetX( newOffsetX );
   
     //scale += event.deltaY * -0.01;
 };
